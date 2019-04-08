b8b4efd2d458b4ca2778e3d76538ab45ad6a3412
Bruno Moreira <comp.brunomoreira@gmail.com>
2019-04-08 10:40:09 -0300

    feature: start text robot

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const readline = require('readline-sync');
-
-function start() {
+const robots = {
+  userInput: require('./robots/user-input.js'),
+  text: require('./robots/text.js'),
+}
+async function start() {
   const content = {};
 
   content.searchTerm = askAndReturnSearchTerm();
   content.prefix = askAndReturnPrefix();
+
+  await robots.text(content);
 
   function askAndReturnSearchTerm() {
     return readline.question('Type a Wikipedia search term: ');
@@ -17,8 +22,8 @@ function start() {
 
     return selectedPrefixText;
   }
-  
-  console.log(content);
+
+  // console.log(content);
 }
 
 start();
